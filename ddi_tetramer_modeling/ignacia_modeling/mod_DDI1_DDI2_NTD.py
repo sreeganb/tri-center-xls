044ab4dec1ceb029357af7e7bc58167af563f21b
Sree Ganesh Balasubramani <sreeganeshbalasubramani@Srees-MacBook-Pro-6.local>
2025-10-15 05:53:43 -0700

    updating the final scripts including the modeling with trifunctional crosslinkers

---
+++ ddi_tetramer_modeling/ignacia_modeling/mod_DDI1_DDI2_NTD.py
@@ -0,0 +1,319 @@
+############################################
+# Modeling ddi2/ddi2 dimer and tetramer
+# occams restraint symmetry
+#
+# iecheverria - Sali Lab - UCSF
+############################################
+
+import IMP
+import RMF
+import IMP.atom
+import IMP.rmf
+import IMP.pmi
+import IMP.pmi.topology
+import IMP.pmi.dof
+import IMP.pmi.macros
+import IMP.pmi.restraints
+import IMP.pmi.restraints.basic
+import IMP.pmi.restraints.stereochemistry
+import IMP.pmi.restraints.crosslinking
+#import IMP.pmi.restraints.occams
+from IMP.pmi.io.crosslink import CrossLinkDataBaseKeywordsConverter
+
+import random
+import numpy as np
+import glob
+import os
+from sys import exit
+from sys import argv
+import pandas as pd
+
+#from ambiguous_xls_restraint_new import *
+###################### SYSTEM SETUP #####################
+
+#top_dir = '/wynton/home/sali/ignacia/ddi_ambiguity/modeling/mod_ddi2_ddi2_tetramer'
+top_dir = './'
+
+mdl = IMP.Model()
+
+#=============================
+# trifunctional crosslinker database
+#============================
+dir = os.getcwd()
+xl_dir = os.path.join(dir,'data')
+xl_fil = os.path.join(xl_dir,'ddi_trifunctional.csv')
+#=============================
+
+cldbkc=CrossLinkDataBaseKeywordsConverter()
+cldbkc.set_protein1_key("Prot A")
+cldbkc.set_protein2_key("Prot B")
+cldbkc.set_residue1_key("Residue A")
+cldbkc.set_residue2_key("Residue B")
+cldbkc.set_unique_id_key("Iid")
+cldbkc.set_psi_key("Score")
+
+cldb=IMP.pmi.io.crosslink.CrossLinkDataBase(cldbkc)
+
+def create_hier_sys(top_file, name = 'sys'):
+    topology = os.path.join(top_dir,top_file)
+    reader = IMP.pmi.topology.TopologyReader(topology,
+                                             pdb_dir = os.path.join(top_dir,'data'),
+                                             fasta_dir = os.path.join(top_dir,'data'))
+
+    
+    bs = IMP.pmi.macros.BuildSystem(mdl,
+                                    resolutions=[1])
+    bs.add_state(reader,
+         keep_chain_id=True)
+
+    hier, dof = bs.execute_macro()
+    mols = bs.get_molecules()[0]
+
+    output = IMP.pmi.output.Output()
+    output.init_rmf(f"ini_{name}.rmf3", [hier])
+    output.write_rmf(f"ini_{name}.rmf3")
+    output.close_rmf(f"ini_{name}.rmf3")
+
+    return hier, dof, mols
+
+def add_connectivity(mols, name = '1'):
+    for molname in mols:
+        for mol in mols[molname]:
+            copy_n = IMP.atom.Copy(mol.get_hierarchy()).get_copy_index()
+            cr = IMP.pmi.restraints.stereochemistry.ConnectivityRestraint(mol)
+            cr.set_label(name+'.'+mol.get_name()+'.'+str(copy_n))
+            cr.add_to_model()
+            output_objects.append(cr)
+            crs.append(cr)
+
+def add_excluded_volume(mols,weight = 1., name = '1'):
+    evr = IMP.pmi.restraints.stereochemistry.ExcludedVolumeSphere(included_objects=mols.values(),
+                                                                  resolution=10)
+    evr.set_label(f'{name}')
+    evr.add_to_model()
+    evr.set_weight(weight)
+    output_objects.append(evr)
+
+def add_crosslinks(file_in,hier, dof, weight = 1., length = 21.0, name = '1'):
+    cldb=IMP.pmi.io.crosslink.CrossLinkDataBase(cldbkc)
+    cldb.create_set_from_file(file_in)
+
+    xls = IMP.pmi.restraints.crosslinking.CrossLinkingMassSpectrometryRestraint(root_hier=hier,
+                                                                                database=cldb,
+                                                                                resolution=1,
+                                                                                length=length,
+                                                                                label=name)
+
+    xls.add_to_model()
+    xls.set_weight(weight)
+    rmf_restraints.append(xls)
+    output_objects.append(xls)
+    dof.get_nuisances_from_restraint(xls)
+
+def add_amb_crosslinks(file_in, hiers, dof, length=21.0, weight = 1., slope = 0.001, name = '1'):
+
+
+    
+    xls = XLs_amb_Restraint(hiers,
+                            file_in,
+                            sigma_init = 3.0,
+                slope = slope,
+                            length = length,
+                            label= name)
+                                                                                
+
+    xls.add_to_model()
+    xls.set_weight(weight)
+    rmf_restraints.append(xls)
+    output_objects.append(xls)
+    dof.get_nuisances_from_restraint(xls)
+
+    return xls
+
+def add_barrier(hier, name = '1'):
+  
+    eb = IMP.pmi.restraints.basic.ExternalBarrier(hierarchies=hier, radius=200)
+    #eb.add_label(f'{name}')
+    eb.add_to_model()
+
+
+def add_Symmetry(hier, dof, selection, weight=1., name = '1'):
+  
+    sy = SymmetryRestraint(hier=hier,
+                           resolution=5,
+                           selection=selection,
+                           label=name)
+
+    sy.add_to_model()
+    sy.set_weight(weight)
+    rmf_restraints.append(sy)
+    output_objects.append(sy)
+    dof.get_nuisances_from_restraint(sy)
+
+def add_COM(hier, dof, selection1, selection2,  distance=30., weight=1., name = '1'):
+  
+    com = COMRestraint(hier=hier,
+                       selection1=selection1,
+                       selection2=selection2,
+                       distance=distance,
+                       label=name)
+
+    com.add_to_model()
+    com.set_weight(weight)
+    rmf_restraints.append(com)
+    output_objects.append(com)
+    dof.get_nuisances_from_restraint(com)
+
+#----------------------------------------------------------------------
+# A new restraint: UpperBoundDistanceRestraint from core module
+#----------------------------------------------------------------------
+def add_distance_restraints_from_csv(file_in, hier, min_distance=0.0, max_distance=19.0, resolution=1.0, kappa=1.0, weight=1.0, name='1'):
+    """
+    Add distance restraints from a CSV file containing crosslink data.
+    
+    Parameters:
+    -----------
+    file_in : str
+        Path to the CSV file containing crosslink data
+    hier : IMP hierarchy
+        The hierarchy to apply restraints to
+    min_distance : float
+        Minimum distance for the restraint (default: 0.0)
+    max_distance : float
+        Maximum distance for the restraint (default: 19.0)
+    resolution : float
+        Resolution for particle selection (default: 1.0)
+    kappa : float
+        Force constant for the restraint (default: 1.0)
+    weight : float
+        Weight for the restraint (default: 1.0)
+    name : str
+        Label for the restraint (default: '1')
+    """
+    # Read crosslink data from CSV
+    xldata = pd.read_csv(file_in, header=0)
+    
+    # Loop through each row in the DataFrame
+    for index, row in xldata.iterrows():
+        c1 = row['Protein1']
+        r1 = row['Residue1']
+        p1 = IMP.atom.Selection(hier, state_index=0, molecule=c1, residue_index=r1, resolution=resolution).get_selected_particles()
+        if not p1:
+            print(f"Warning: No particles found for {c1} residue {r1}")
+            continue
+        
+        c2 = row['Protein2']
+        r2 = row['Residue2']
+        p2 = IMP.atom.Selection(hier, state_index=0, molecule=c2, residue_index=r2, resolution=resolution).get_selected_particles()
+        if not p2:
+            print(f"Warning: No particles found for {c2} residue {r2}")
+            continue
+        
+        # Create tuple format for DistanceRestraint
+        tup1 = [r1, r1, c1, 0]
+        tup2 = [r2, r2, c2, 0]
+        
+        # Create and add distance restraint
+        disres = IMP.pmi.restraints.basic.DistanceRestraint(hier, tup1, tup2, min_distance, max_distance, resolution=resolution, kappa=kappa)
+        disres.set_label(f'{name}.{c1}_{r1}-{c2}_{r2}')
+        disres.add_to_model()
+        disres.set_weight(weight)
+        output_objects.append(disres)
+        
+    print(f"Added {len(xldata)} distance restraints from {file_in}")
+
+#----------------------------------------------------------------------
+###############################
+# All species 
+###############################
+hier_S1, dof_S1, mols_S1 = create_hier_sys(f'{top_dir}/top_DDI1_NTD.dat', name = 'tetramer1')
+hier_S2, dof_S2, mols_S2 = create_hier_sys(f'{top_dir}/top_DDI2_NTD.dat', name = 'tetramer2')
+
+##############################
+# Combined hierarchy
+##############################
+
+p = IMP.Particle(mdl)
+hier_all = IMP.atom.Hierarchy.setup_particle(p)
+hier_all.add_child(hier_S1)
+hier_all.add_child(hier_S2)
+hier_all.set_name('System')
+
+states = IMP.atom.get_by_type(hier_all,IMP.atom.STATE_TYPE)
+print('All states:', states)
+
+##############################
+# Connectivity
+##############################
+output_objects = [] 
+sample_objects = []
+rmf_restraints = []
+
+crs = []
+
+add_connectivity(mols_S1, name='tetramer1')
+add_connectivity(mols_S2, name='tetramer2')
+
+##############################
+# Excluded Volume
+##############################
+add_excluded_volume(mols_S1, name='tetramer1')
+add_excluded_volume(mols_S2, name='tetramer2')
+
+##############################
+# External barrier
+##############################
+
+add_barrier(hier_S1, name = 'tetramer1')
+add_barrier(hier_S2, name = 'tetramer2')
+
+##############################
+# XLs
+##############################
+
+#xls_tetramer = f'{top_dir}/data/mixed_DDI1_dimer.csv'
+#add_amb_crosslinks(xls_tetramer, [hier_S1], dof_S1, weight = 5.0, length = 21., slope=0.01 ,name='tetramer1')
+
+#xls_tetramer = f'{top_dir}/data/mixed_DDI2_dimer.csv'
+#add_amb_crosslinks(xls_tetramer, [hier_S2], dof_S2, weight = 5.0, length = 21., slope=0.01 ,name='tetramer2')
+
+##############################
+# Distance Restraints from CSV
+##############################
+add_distance_restraints_from_csv(xl_fil, hier_S1, min_distance=0.0, max_distance=19.0, resolution=1.0, kappa=1.0, weight=1.0, name='tetramer1_distance')
+add_distance_restraints_from_csv(xl_fil, hier_S2, min_distance=0.0, max_distance=19.0, resolution=1.0, kappa=1.0, weight=1.0, name='tetramer2_distance')
+
+##############################
+# Write everything
+##############################  
+    
+output = IMP.pmi.output.Output()
+output.init_rmf("ini_all.rmf3", [hier_all])
+output.write_rmf("ini_all.rmf3")
+output.close_rmf("ini_all.rmf3")
+
+##############################
+# Shuffle
+##############################    
+IMP.pmi.tools.shuffle_configuration(hier_S1, max_translation=100)
+IMP.pmi.tools.shuffle_configuration(hier_S2, max_translation=100)
+
+movers_all = dof_S1.get_movers() + dof_S2.get_movers()
+
+##############################
+# MC
+##############################    
+
+rex=IMP.pmi.macros.ReplicaExchange(mdl,
+                                   root_hier=hier_all,                                        
+                                   monte_carlo_sample_objects=movers_all,   
+                                   replica_exchange_maximum_temperature=4.0,
+                                   global_output_directory="output/",
+                                   output_objects=output_objects,
+                                   monte_carlo_steps=20,
+                                   number_of_frames=10000,
+                                   number_of_best_scoring_models=0)
+
+rex.execute_macro()
+
+exit()
